63eaaf47017f1656b9687929e7ec4b81f4e78494
James Bruton <xrobots@gmail.com>
2019-02-04 16:32:20 +0000

    Add files via upload

---
+++ Eggs/MEGA/MEGA.ino
@@ -0,0 +1,173 @@
+
+#include <OneWire.h>                  // for temp densor
+#include <DallasTemperature.h>        // for temp densor
+
+#include <Wire.h>                     // for LCD
+#include <LiquidCrystal_I2C.h>        // for LCD
+
+#include <Adafruit_TiCoServo.h>       // Adafruit servo library
+
+#define SERVO_MIN 800 // 1 ms pulse
+#define SERVO_MAX 2300 // 2 ms pulse
+
+Adafruit_TiCoServo servo1;
+Adafruit_TiCoServo servo2;
+
+// Set the pins on the I2C chip used for LCD connections (Some LCD use Address 0x27 and others use 0x3F):
+LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Set the LCD I2C address (addr, en, rw, rs, d4, d5, d6, d7, backlight, polarity)
+
+// Data wire of temp sensor is plugged into pin 2 on the Arduino 
+#define ONE_WIRE_BUS 8 
+
+// Setup a oneWire instance to communicate with any OneWire devices  
+// (not just Maxim/Dallas temperature ICs) 
+OneWire oneWire(ONE_WIRE_BUS); 
+
+// Pass our oneWire reference to Dallas Temperature. 
+DallasTemperature sensors(&oneWire);
+
+unsigned long previousMillis = 0;
+const long interval = 20;
+
+int yellow;           // buttons
+int red;
+int green1;
+int green2;
+
+int state = 0;        // stage of the automation
+
+long bookmarktime;          // the time at the start
+long timeleft;              // the time left as the couinter counts down
+
+void setup() {
+
+    pinMode(2, INPUT_PULLUP);
+    pinMode(3, INPUT_PULLUP);
+    pinMode(4, INPUT_PULLUP);
+    pinMode(5, INPUT_PULLUP);
+
+    pinMode(37, OUTPUT);
+    pinMode(39, OUTPUT);
+
+    digitalWrite(37, LOW);        // low for up  
+    digitalWrite(39, HIGH);       // low for down
+
+    servo1.attach(44, SERVO_MIN, SERVO_MAX);
+    servo2.attach(45, SERVO_MIN, SERVO_MAX);
+
+    servo1.write(8);   // sive
+    servo2.write(180);    // base
+    
+    Serial.begin(9600); // start serial port     
+    
+    sensors.begin(); // Start up the temp sensor library 
+
+    lcd.begin(20,4);   // Initialize the lcd for 20 chars 4 lines, turn on backlight
+  
+    // NOTE: Cursor Position: (CHAR, LINE) starts at 0  
+    lcd.setCursor(0,0);
+    lcd.print("BreakFast Machine   ");
+    lcd.setCursor(0,1);
+    lcd.print("Ready for EGGS ??!! ");
+    lcd.setCursor(0,2);
+    lcd.print("Water temp:");
+    lcd.setCursor(0,3);
+    lcd.print("Yellow to start     ");
+} 
+
+
+void loop() { 
+
+   unsigned long currentMillis = millis();
+   if (currentMillis - previousMillis >= interval) {  // start timed event for read and send
+      previousMillis = currentMillis;
+    
+          // call sensors.requestTemperatures() to issue a global temperature 
+          // request to all devices on the bus         
+          //Serial.print(" Requesting temperatures..."); 
+          sensors.requestTemperatures(); // Send the command to get temperature readings 
+          //Serial.println("DONE"); 
+          //Serial.print("Temperature is: "); 
+          //Serial.print(sensors.getTempCByIndex(0)); // Why "byIndex"?  
+          // You can have more than one DS18B20 on the same bus.  
+          // 0 refers to the first IC on the wire 
+          lcd.setCursor(12,2);
+          lcd.print(sensors.getTempCByIndex(0)); 
+
+          yellow = digitalRead(2);
+          red = digitalRead(3);
+          green1 = digitalRead(4);
+          green2 = digitalRead(5);
+
+          if (state == 0 && yellow == 0) {
+              servo1.write(8);   // sive
+              digitalWrite(37, LOW);        // low for up  
+              digitalWrite(39, HIGH);       // low for down
+              lcd.setCursor(0,3);
+              lcd.print("Waiting for water   ");
+              state = 1;
+          }
+
+          else if (state == 1 && sensors.getTempCByIndex(0) > 90) {
+              lcd.setCursor(0,3);
+              lcd.print("Water at temperature");
+              //move robot arm stuff here
+              servo1.write(8);   // sive
+              servo2.write(0);    // base
+              delay(2000);
+              digitalWrite(37, HIGH);        // low for up  
+              digitalWrite(39, LOW);       // low for down
+              delay(2000);
+              state = 2;
+          }
+
+          else if (state == 2) {
+            bookmarktime = millis()  +  360000;                     // set the clock for 5 minutes in the future in millisseconds (1000 x 60 seconds x 6 minutes)
+            state = 3;
+          }
+
+          else if (state == 3) {
+              timeleft = bookmarktime - millis();                   // work out the time left on every cycle based on the actual time that's expired according to the Arduino internal clock
+              lcd.setCursor(0,3);
+              lcd.print("Time left:          ");
+              lcd.setCursor(11,3);
+              lcd.print(timeleft);
+                if (timeleft <= 0) {
+                    state = 4;
+              }
+          }
+
+          else if (state == 4) {
+              lcd.setCursor(0,3);
+              lcd.print("Eggs are ready!!    ");
+              // move robot arm stuff
+              digitalWrite(37, LOW);        // low for up  
+              digitalWrite(39, HIGH);       // low for down
+              delay(2000);
+              servo1.write(8);   // sive
+              servo2.write(180);    // base
+              delay(1000);
+              servo1.write(120);   // sive
+              state = 5;             
+          }
+
+          else if (state == 5) {
+              lcd.setCursor(0,3);
+              lcd.print("Press red for reset");
+              if (red == 0) {
+                state = 6;
+                servo1.write(8);   // sive
+              } 
+          }
+
+          else if (state == 6) {
+              lcd.setCursor(0,3);
+              lcd.print("Yellow to start     ");
+              state = 0;
+          }         
+
+          
+
+   }  // end of timed event
+   
+}   // end of main loop
---
+++ Toast/MEGA/MEGA.ino
@@ -0,0 +1,336 @@
+#include <Wire.h>                     // for LCD
+#include <LiquidCrystal_I2C.h>        // for LCD
+
+#include <PID_v1.h>  //PID loop from http://playground.arduino.cc/Code/PIDLibrary
+
+#include <Servo.h>
+
+Servo servoTimer;
+Servo servoDoor;
+Servo servoFork1;
+Servo servoFork2;
+
+int servoTimerPos;
+int servoDoorPos;
+int servoFork1Pos;
+int servoFork2Pos;
+
+int motorPos;
+int motorPos1;
+
+int servoTimerPos_filtered;
+int servoDoorPos_filtered;
+int servoFork1Pos_filtered;
+int servoFork2Pos_filtered;
+
+int motorPos_filtered;
+
+double Pk1 = 15;  //speed it gets there
+double Ik1 = 0;
+double Dk1 = 0;
+double Setpoint1, Input1, Output1, Output1a;    // PID variables wheel
+
+PID PID1(&Input1, &Output1, &Setpoint1, Pk1, Ik1 , Dk1, DIRECT);
+
+int pot;
+
+// Set the pins on the I2C chip used for LCD connections (Some LCD use Address 0x27 and others use 0x3F):
+LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Set the LCD I2C address (addr, en, rw, rs, d4, d5, d6, d7, backlight, polarity) 
+
+unsigned long previousMillis = 0;
+const long interval = 20;
+
+unsigned long previousOvenMillis = 0;
+
+int red;        // buttons
+int white;
+
+int state = 0;        // stage of the automation
+
+
+
+void setup() {
+
+    servoTimer.attach(10);
+    servoDoor.attach(11);
+    servoFork1.attach(44);
+    servoFork2.attach(46);
+
+    pinMode(22, INPUT_PULLUP);    // red
+    pinMode(24, INPUT_PULLUP);    // white
+
+    pinMode(A0, INPUT);           // analog pot
+
+    pinMode(2, OUTPUT);           // pwm
+    pinMode(3, OUTPUT);           // pwm
+
+    PID1.SetMode(AUTOMATIC);              
+    PID1.SetOutputLimits(-255, 255);
+    PID1.SetSampleTime(20);
+    
+    Serial.begin(115200); // start serial port   
+    
+    lcd.begin(20,4);   // Initialize the lcd for 20 chars 4 lines, turn on backlight
+  
+    // NOTE: Cursor Position: (CHAR, LINE) starts at 0  
+    lcd.setCursor(0,0);
+    lcd.print("BreakFast Machine   ");
+    lcd.setCursor(0,1);
+    lcd.print("Ready for TOAST ?!  ");
+    lcd.setCursor(0,2);
+    lcd.print("                    ");
+    lcd.setCursor(0,3);
+    lcd.print("Press red to start  ");
+
+    servoTimerPos = 0;             // 0 off - 180 on           
+    servoDoorPos = 180;               // 83 open - 160 closed
+    servoFork1Pos = 151;               // left fork. Lower number toward oven
+    servoFork2Pos = 20;              // right fork. Higher number toward oven
+    Setpoint1 = 125;          // 125 - 250. high number is closer to oven
+
+    Serial.println("Ready!");
+    Serial.println("'a' to open door");
+    Serial.println("'b' to close door");
+    Serial.println("'c' to move tray back");
+    Serial.println("'d' to move tray forward");
+    Serial.println("'e' to move forks down");
+    Serial.println("'f' to move forks mid");
+    Serial.println("'g' to move forks tip");
+    Serial.println("'h' to set timer");
+    Serial.println("'i' to release timer");
+} 
+
+
+void loop() { 
+
+   unsigned long currentMillis = millis();
+   if (currentMillis - previousMillis >= interval) {  // start timed event for read and send
+      previousMillis = currentMillis; 
+
+         if (Serial.available()) {
+                char c = Serial.read();
+
+                if (c == 'a') {
+                    servoDoorPos = 87;
+                }
+                else if (c == 'b') {
+                    servoDoorPos = 180;
+                }
+                else if (c == 'f') {
+                    servoFork1Pos = 36;
+                    servoFork2Pos = 134;
+                }
+                else if (c == 'e') {
+                    servoFork1Pos = 19;
+                    servoFork2Pos = 150;
+                }
+                else if (c == 'g') {
+                    servoFork1Pos = 151;
+                    servoFork2Pos = 20;
+                }
+                else if (c == 'h') {
+                    servoTimerPos = 180;                    
+                }
+                else if (c == 'i') {
+                    servoTimerPos = 0;                    
+                }
+                else if (c == 'c') {
+                    Setpoint1 = 125;          // 125 - 250. high number is closer to oven                   
+                }
+                else if (c == 'd') {
+                    Setpoint1 = 250;          // 125 - 250. high number is closer to oven                 
+                }
+
+         }
+
+         red = digitalRead(22);
+         white = digitalRead(24);
+
+         // state machine stuff in here
+
+         lcd.setCursor(0,2);
+         lcd.print((currentMillis - previousOvenMillis)/1000);
+
+         if (state == 0 && red == 0) {
+             servoTimerPos = 180;
+             Setpoint1 = 125;          // 125 - 250. high number is closer to oven 
+             state = 1;
+         }
+
+         else if (state == 1 && currentMillis - previousOvenMillis > 1000) {
+             servoFork1Pos = 151;
+             servoFork2Pos = 20;             
+             lcd.setCursor(0,3);
+             lcd.print("Preheating oven     ");
+             previousOvenMillis = currentMillis;
+             state = 2;
+         }
+
+         else if (state == 2 && currentMillis - previousOvenMillis > 30000){     // preheat timer 1 min
+              servoDoorPos = 87;
+              previousOvenMillis = currentMillis;
+              state = 3;
+         }
+
+         else if (state == 3 && currentMillis - previousOvenMillis > 1000){
+              servoFork1Pos = 36;               // forks ready for food
+              servoFork2Pos = 134;
+              lcd.setCursor(0,3);
+              lcd.print("Insert food         ");
+              state = 4;
+         }
+         
+         else if (state == 4 && red == 0) {
+              Setpoint1 = 250;          // move tray into oven
+              state = 5;
+              previousOvenMillis = currentMillis;
+         }
+
+         else if (state == 5 && currentMillis - previousOvenMillis > 2000) {
+              servoFork1Pos = 19;      // lower tray
+              servoFork2Pos = 150;
+              state = 6;
+              previousOvenMillis = currentMillis;
+         }
+
+         else if (state == 6 && currentMillis - previousOvenMillis > 2000) {
+              Setpoint1 = 125;          // move carriage away from oven
+              state = 7;
+              previousOvenMillis = currentMillis;
+         }
+
+         else if (state == 7 && currentMillis - previousOvenMillis > 2000) {
+              servoFork1Pos = 151;      // raise forks away from door.
+              servoFork2Pos = 20;
+              state = 8;
+              previousOvenMillis = currentMillis;
+         }
+
+         else if (state == 8 && currentMillis - previousOvenMillis > 1000) {
+              servoDoorPos = 180;       // close door
+              state = 9;
+              previousOvenMillis = currentMillis;
+         }
+
+         else if (state == 9 && currentMillis - previousOvenMillis > 1000) {
+              servoTimerPos = 0;        // release timer
+              state = 10;
+              previousOvenMillis = currentMillis;
+         }
+
+         else if (state == 10 && currentMillis - previousOvenMillis > 30000) {
+              servoDoorPos = 87;        // open door
+              state = 11;
+              previousOvenMillis = currentMillis;
+         }
+
+         else if (state == 11 && currentMillis - previousOvenMillis > 2000) {
+              servoFork1Pos = 19;       // forks ready to pick up tray
+              servoFork2Pos = 150;
+              state = 12;
+              previousOvenMillis = currentMillis;
+         }
+
+         else if (state == 12 && currentMillis - previousOvenMillis > 2000) {
+              Setpoint1 = 250;          // move carrieage into oven
+              state = 13;
+              previousOvenMillis = currentMillis;
+         }
+
+         else if (state == 13 && currentMillis - previousOvenMillis > 2000) {
+              servoFork1Pos = 36;               // raise forks
+              servoFork2Pos = 134;
+              state = 14;
+              previousOvenMillis = currentMillis;
+         }
+
+         else if (state == 14 && currentMillis - previousOvenMillis > 3000) {
+              Setpoint1 = 125;          // move carriage away from oven
+              state = 15;
+              previousOvenMillis = currentMillis;
+         }
+
+         else if (state == 15 && currentMillis - previousOvenMillis > 7000) {
+              servoFork1Pos = 151;      // throw out food
+              servoFork2Pos = 20;
+              state = 16;
+              previousOvenMillis = currentMillis;
+         }
+
+         else if (state == 16 && currentMillis - previousOvenMillis > 3000) {
+              servoDoorPos = 180;       // close door
+              state = 0;
+              lcd.setCursor(0,3);
+              lcd.print("Press red to start  ");
+              previousOvenMillis = currentMillis;
+         }
+
+         
+
+
+
+
+
+
+
+
+
+
+
+
+         // end  of state machine
+
+          pot = analogRead(A0);
+
+          servoTimerPos_filtered = filter(servoTimerPos, servoTimerPos_filtered, 10);
+          servoDoorPos_filtered = filter(servoDoorPos, servoDoorPos_filtered, 20);
+          servoFork1Pos_filtered = filter(servoFork1Pos, servoFork1Pos_filtered, 10);
+          servoFork2Pos_filtered = filter(servoFork2Pos, servoFork2Pos_filtered, 10);
+
+          servoTimer.write(servoTimerPos_filtered);
+          servoDoor.write(servoDoorPos_filtered);
+          servoFork1.write(servoFork1Pos_filtered);
+          servoFork2.write(servoFork2Pos_filtered);
+
+          Input1 = pot;
+          
+          PID1.Compute();
+
+          motorPos_filtered = filter(Output1, motorPos_filtered, 15);
+
+          // drive motor
+          
+          if (motorPos_filtered < -100)                                       // decide which way to turn the motor
+          {
+            motorPos1 = abs(motorPos_filtered);
+            analogWrite(2, motorPos1);                           // set PWM pins 
+            analogWrite(3, 0);
+          }
+          else if (motorPos_filtered > 100)                                  // decide which way to turn the motor
+          { 
+            motorPos1 = abs(motorPos_filtered);
+            analogWrite(3, motorPos1);  
+            analogWrite(2, 0);
+          } 
+          else
+          {
+            analogWrite(2, 0);  
+            analogWrite(3, 0);
+          }  
+
+          
+
+          
+
+   }  // end of timed event
+   
+}   // end of main loop
+
+
+
+//***************filter  motions*****************
+
+double filter(double lengthOrig, double currentValue, int filter) {
+  double lengthFiltered =  (lengthOrig + (currentValue * filter)) / (filter + 1);
+  return lengthFiltered;  
+}
